d413a670b68260ad94325b9fd8e426c493cbeb58
Pedro Marinho <pedromarinho@MacBook-Air-de-Pedro.local>
2024-03-16 11:26:14 -0300

    initial commit

---
+++ src/main/java/br/com/pedro/springbootcognito/SpringBootCognitoApplication.java
@@ -0,0 +1,13 @@
+package br.com.pedro.springbootcognito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCognitoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCognitoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/pedro/springbootcognito/SpringBootCognitoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pedro.springbootcognito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCognitoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

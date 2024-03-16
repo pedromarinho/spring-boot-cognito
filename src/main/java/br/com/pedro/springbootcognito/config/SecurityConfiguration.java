0a0bd720184601a0e8364174fb31b77097f1551b
Pedro Marinho <pedromarinho@MacBook-Air-de-Pedro.local>
2024-03-16 16:11:57 -0300

    configure oauth2 resource server

---
+++ src/main/java/br/com/pedro/springbootcognito/config/SecurityConfiguration.java
@@ -0,0 +1,26 @@
+package br.com.pedro.springbootcognito.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(req -> {
+                    req.requestMatchers("/actuator/**").permitAll();
+                    req.anyRequest().authenticated();
+                })
+                .oauth2ResourceServer(spec -> spec.jwt(Customizer.withDefaults()))
+                .build();
+    }
+}
